1ee1bbd56c88ed0b56b4d293d52b7feaa08f027f
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-23 23:21:24 -0700

    Modifying log config

--- actions/homework-setup/run_test.sh
+++ actions/homework-setup/run_test.sh
@@ -1,3 +1,3 @@
 echo ""
 echo "Running Group ${1} Tests..."
-mvn -f pom.xml -ntp "-Dgroups=${1}" "-Dorg.slf4j.simpleLogger.levelInBrackets=false" "-Dorg.slf4j.simpleLogger.showLogName=true" test
+mvn -f pom.xml -ntp "-Dgroups=${1}" test
