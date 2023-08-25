56e914c6ebb14dfa3bcb01d2318a0d0df435990a
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 20:41:57 -0700

    Fixing options

--- homework/check-commits/index.js
+++ homework/check-commits/index.js
@@ -5,21 +5,20 @@ module.exports = async ({core, exec}) => {
     await exec.exec('git log --oneline refs/remotes/origin/main');
     core.endGroup();
 
+    // capture output for number of commits
     let num_commits = '';
-    await exec.exec('git rev-list --count refs/remotes/origin/main', {
-      listeners: { stdout: (data: Buffer) => { num_commits = data.toString(); } }
-    });
+    
+    const options = {};
+    options.listeners = {
+      stdout: (data: Buffer) => {
+        num_commits = data.toString().trim();
+      }
+    };
+
+    await exec.exec('git rev-list --count refs/remotes/origin/main', options);
 
     num_commits = parseInt(num_commits);
     core.info(`Found at least ${num_commits} commits.`);
-    
-    // NUM_COMMITS=$(git rev-list --count refs/remotes/origin/main)
-    // echo "NUM_COMMITS=${NUM_COMMITS}" >> $GITHUB_ENV
-    // echo "num_commits=${NUM_COMMITS}" >> $GITHUB_OUTPUT
-    // echo "::endgroup::"
-
-    // echo ""
-    // echo "Found Commits: ${NUM_COMMITS} (at least)"
 
     const min_commits = parseInt(process.env.MIN_COMMITS);
     // const num_commits = parseInt(process.env.NUM_COMMITS);
