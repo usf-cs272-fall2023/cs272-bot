dbf4f76a1f269ac87070289dd302c54e61b97ee4
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 20:38:50 -0700

    Adding exec

--- homework/check-commits/index.js
+++ homework/check-commits/index.js
@@ -1,7 +1,28 @@
-module.exports = async ({core}) => {
+module.exports = async ({core, exec}) => {
   try {
+    core.startGroup('Listing commits...');
+    await exec.exec('git fetch --unshallow');
+    await exec.exec('git log --oneline refs/remotes/origin/main');
+    core.endGroup();
+
+    let num_commits = '';
+    await exec.exec('git rev-list --count refs/remotes/origin/main', {
+      listeners: { stdout: (data: Buffer) => { num_commits = data.toString(); } }
+    });
+
+    num_commits = parseInt(num_commits);
+    core.info(`Found at least ${num_commits} commits.`);
+    
+    // NUM_COMMITS=$(git rev-list --count refs/remotes/origin/main)
+    // echo "NUM_COMMITS=${NUM_COMMITS}" >> $GITHUB_ENV
+    // echo "num_commits=${NUM_COMMITS}" >> $GITHUB_OUTPUT
+    // echo "::endgroup::"
+
+    // echo ""
+    // echo "Found Commits: ${NUM_COMMITS} (at least)"
+
     const min_commits = parseInt(process.env.MIN_COMMITS);
-    const num_commits = parseInt(process.env.NUM_COMMITS);
+    // const num_commits = parseInt(process.env.NUM_COMMITS);
 
     // if (num_commits < min_commits) {
       core.error(`Found only ${num_commits} commit(s)... at least ${min_commits} commits are required.`, {'title': '-5 Points'});
