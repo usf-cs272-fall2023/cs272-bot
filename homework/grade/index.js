00092d45c63fa3b07898b1003ce6226936670aa2
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-25 08:51:55 -0700

    Adjusting output

--- homework/grade/index.js
+++ homework/grade/index.js
@@ -9,83 +9,66 @@ module.exports = async ({github, context, core}) => {
     const submitted_text = grade_results.submitted_text;
     const deadline_text = grade_results.deadline_text;
 
+    // output results of late penalties
+    if (late_points > 0) {
+      core.warning(`Submitted ${late_multiplier} day(s) late (submitted: ${submitted_text}, deadline: ${deadline_text}).`, {'title': `-${late_points} Points`});
+    }
+
     // output points earned from tests before handle deductions
     core.notice(`Earned ${grade_starting} points from tests before any deductions.`, {'title': `+${grade_starting} Points`});
 
-    if (late_points > 0) {
-      core.warning(`Submitted ${late_multiplier} day(s) late (submitted: ${submitted_text}, deadline: ${deadline_text}).`, {'title': `-${late_points}`});
-    }
+    // calculate final grade
+    let deductions = late_points;
+    deductions += process.env.COMPILE_STATUS !== 'success' ? 5 : 0;
+    deductions += process.env.JAVADOC_STATUS !== 'success' ? 5 : 0;
+    deductions += process.env.COMMITS_STATUS !== 'success' ? 5 : 0;
 
-    // core.info(`Deducted grade by -${process.env.LATE_POINTS} due to late penalty.`);
+    core.setOutput('deductions', deductions);
+    core.info(`Total deductions: ${deductions}`);
 
-    // if (process.env.COMPILE_STATUS !== 'success') {
-    //   grade_points -= 5;
-    //   core.info('Deducted grade by -5 points due to compile warnings.');
-    // }
+    let final_points = deductions > grade_starting ? 0 : grade_starting - deductions;
+    core.setOutput('final_points', final_points);
+    core.info(`Final points: ${final_points}`);
 
-    // if (process.env.JAVADOC_STATUS !== 'success') {
-    //   grade_points -= 5;
-    //   core.info('Deducted grade by -5 points due to Javadoc warnings.');
-    // }
+    const final_grade = `Points ${final_points}/${grade_possible}`;
 
-    // if (process.env.COMMITS_STATUS !== 'success') {
-    //   grade_points -= 5;
-    //   core.info('Deducted grade by -5 points due to too few commits.');
-    // }
+    // trick github classroom into using this new value
+    const workflow_run = await github.rest.actions.getWorkflowRun({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      run_id: context.runId
+    });
 
-    // if (grade_points < 0) {
-    //   grade_points = 0;
-    //   core.info(`Points reset to 0 (was ${grade_points}).`);
-    // }
-
-    // const final_points = grade_points;
-    // const final_grade = `Points ${final_points}/${process.env.GRADE_POSSIBLE}`;
-    // core.setOutput('final_points', final_points);
-
-    // core.info(final_points);
-
-    // // trick github classroom into using this new value
-    // const workflow_run = await github.rest.actions.getWorkflowRun({
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   run_id: context.runId
-    // });
-
-    // const suite_url = workflow_run.data.check_suite_url;
-    // const suite_id = parseInt(suite_url.match(/[0-9]+$/)[0], 10);
+    const suite_url = workflow_run.data.check_suite_url;
+    const suite_id = parseInt(suite_url.match(/[0-9]+$/)[0], 10);
     
-    // const check_runs = await github.rest.checks.listForSuite({
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   check_name: 'Autograding',
-    //   check_suite_id: suite_id,
-    // });
+    const check_runs = await github.rest.checks.listForSuite({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      check_name: 'Autograding',
+      check_suite_id: suite_id,
+    });
   
-    // let text = process.env.POINTS;
-    // text = 'Points 10/5';
-    
-    // await github.rest.checks.update({
-    //   owner: context.repo.owner,
-    //   repo: context.repo.repo,
-    //   check_run_id: check_runs.data.check_runs[0].id,
-    //   output: {
-    //     title: 'Autograding',
-    //     summary: text,
-    //     text: text,
-    //     annotations: [
-    //       {
-    //         path: '.github',
-    //         start_line: 1,
-    //         end_line: 1,
-    //         annotation_level: 'notice',
-    //         message: text,
-    //         title: 'Autograding complete',
-    //       },
-    //     ],
-    //   },
-    // });
-
-
+    await github.rest.checks.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      check_run_id: check_runs.data.check_runs[0].id,
+      output: {
+        title: 'Autograding',
+        summary: 'Points 1/2',
+        text: 'Points 2/3',
+        annotations: [
+          {
+            path: '.github',
+            start_line: 1,
+            end_line: 1,
+            annotation_level: 'notice',
+            message: 'Points 3/4',
+            title: 'Autograding complete',
+          },
+        ],
+      },
+    });
   }
   catch(error) {
     core.info(`${error.name}: ${error.message}`);
