15c50699b9e513ecdd7b9e1327562312af48479b
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 23:03:18 -0700

    Output

--- homework/grade/index.js
+++ homework/grade/index.js
@@ -2,6 +2,8 @@ module.exports = async ({github, context, core}) => {
   try {
     const grades = process.env.GRADE_OUTPUT;
 
+    console.log(JSON.stringify(grades));
+
     const grade_starting = grades.grade_starting;
     core.notice(`Earned ${grade_starting} from tests before any deductions`, {'title': `+${grade_starting} Points`});
   }
