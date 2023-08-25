448750d27b980ea88890b8f4440be512f7115ba7
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 22:56:30 -0700

    Calculating grade skeleton

---
+++ homework/grade/index.js
@@ -0,0 +1,12 @@
+module.exports = async ({github, context, core}) => {
+  try {
+    const grades = process.env.GRADE_OUTPUT;
+
+    const grade_starting = grades.grade_starting;
+    core.notice(`Earned ${grade_starting} from tests before any deductions`, {'title': `+${grade_starting} Points`});
+  }
+  catch(error) {
+    core.info(`${error.name}: ${error.message}`);
+    core.setFailed('Unable to calculate final grade after deductions.');
+  }
+};
