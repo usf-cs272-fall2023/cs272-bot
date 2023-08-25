27f116ea728d7c89c9495f8564f2ea5acae61105
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 22:03:29 -0700

    Adding deadline lookup

--- actions/check-deadline/index.js
+++ actions/check-deadline/index.js
@@ -1,10 +1,31 @@
-module.exports = async ({core, exec, constants}) => {
+module.exports = async ({github, context, core, constants, DateTime, Settings}) => {
   try {
-    core.info(process.env.ASSIGNMENT_NAME);
-    core.info(JSON.stringify(constants));
+    const zone = 'America/Los_Angeles';
+    const eod = 'T23:59:59';
+    Settings.defaultZone = zone;
+
+    const assignment_name = process.env.ASSIGNMENT_NAME;
+
+    if (!(assignment_name in constants.deadlines)) {
+      throw new Error(`Unrecognized assignment: ${assignment_name}`);
+    }
+  
+    const assignment = constants.deadlines[assignment_name];
+    console.log(`Assignment: ${assignment_name}`);
+  
+    // process deadline and possible extension
+    let deadline_date = DateTime.fromISO(`${assignment.due}${eod}`);
+  
+    if (!deadline_date.isValid) {
+      throw new Error(`Unable to parse deadline date: ${assignment.due}${eod} (${deadline_date.invalidReason})`);
+    }
+  
+    let deadline_text = deadline_date.toLocaleString(DateTime.DATETIME_FULL);
+    console.log(`  Deadline: ${deadline_text}`);
+ 
   }
   catch(error) {
     core.info(`${error.name}: ${error.message}`);
-    core.setFailed('Unable to check the number of commits.');
+    core.setFailed('Unable to check the deadline and calculate a late penalty.');
   }
 };
