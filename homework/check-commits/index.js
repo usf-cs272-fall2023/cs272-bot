d0bc45e2e609c0ca407d30dd1c9ddcdc6afa3b53
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-24 20:25:23 -0700

    Changing output

--- homework/check-commits/index.js
+++ homework/check-commits/index.js
@@ -4,11 +4,11 @@ module.exports = async ({github, context, core, exec}) => {
     const num_commits = parseInt(process.env.NUM_COMMITS);
 
     // if (num_commits == min_commits) {
-      core.setFailed(`Found only ${num_commits} commit(s)... at least ${min_commits} commits are required (-5 points).`);
+      core.setFailed(`Found only ${num_commits} commit(s)... at least ${min_commits} commits are required.`, {'title': '-5 Points'});
     // }
   }
   catch(error) {
     core.info(`${error.name}: ${error.message}`);
-    core.setFailed('Unable to check the number of commits.');
+    core.setFailed('Unable to check the number of commits.', {'title': 'Error'});
   }
 };
