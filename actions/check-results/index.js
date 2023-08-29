b7137b845a94bd56a44981954a04c7dd3d9828eb
Sophie Engle <sjengle@cs.usfca.edu>
2023-08-28 21:13:39 -0700

    Removing workflow run

--- actions/check-results/index.js
+++ actions/check-results/index.js
@@ -6,7 +6,6 @@ module.exports = async ({github, context, core, fetch, AdmZip}) => {
     core.info(process.env.ARTIFACT_NAME);
     core.info(process.env.ARTIFACT_JSON);
     core.info(process.env.WORKFLOW_NAME);
-    core.info(process.env.WORKFLOW_RUN);
   }
   catch(error) {
     core.info(`${error.name}: ${error.message}`);
