f2782cad028c65159f86d28f0c58b968410f2df4
Sophie Engle <sjengle@cs.usfca.edu>
2023-12-10 17:19:25 -0800

    Testing summary

--- .github/scripts/stats-process.js
+++ .github/scripts/stats-process.js
@@ -1,20 +1,27 @@
 module.exports = async ({github, context, core}) => {
   const {RELEASES} = process.env;
-  let markdown = [];
-  markdown.push('# CS 272 Project Statistics');
-  markdown.push('');
-  markdown.push(`:octocat: Hello @${ context.actor }! You can find your project statistics below.`);
-  markdown.push('');
+  let summary = core.summary;
 
-  let projects = {
-    project1: 'Project 1 Index',
-    project2: 'Project 2 Search',
-    project3: 'Project 3 Threads',
-    project4: 'Project 4 Crawl',
-    project5: 'Project 5 Server'
-  };
+  summary = summary.addRaw('# CS 272 Project Statistics', true);
 
-  
+  // let markdown = [];
+  // markdown.push('# CS 272 Project Statistics');
+  // markdown.push('');
+  // markdown.push(`:octocat: Hello @${ context.actor }! You can find your project statistics below.`);
+  // markdown.push('');
 
-  await core.summary.addRaw(markdown.join('\n')).write();
+  // let projects = {
+  //   project1: 'Project 1 Index',
+  //   project2: 'Project 2 Search',
+  //   project3: 'Project 3 Threads',
+  //   project4: 'Project 4 Crawl',
+  //   project5: 'Project 5 Server'
+  // };
+
+  // for (const project in projects) {
+
+  // }
+
+  // await core.summary.addRaw(markdown.join('\n')).write();
+  await summary.write();
 };
