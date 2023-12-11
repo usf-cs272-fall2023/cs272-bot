28f48e1952a25d4def9f58b1c4eed2d0b4c7a702
Sophie Engle <sjengle@cs.usfca.edu>
2023-12-10 17:26:36 -0800

    Adding project loop

--- .github/scripts/stats-process.js
+++ .github/scripts/stats-process.js
@@ -7,18 +7,22 @@ module.exports = async ({github, context, core}) => {
   summary = summary.addRaw(`:octocat: Hello @${ context.actor }! You can find your project statistics below.`, true);
   summary = summary.addRaw('', true);
 
-  // let projects = {
-  //   project1: 'Project 1 Index',
-  //   project2: 'Project 2 Search',
-  //   project3: 'Project 3 Threads',
-  //   project4: 'Project 4 Crawl',
-  //   project5: 'Project 5 Server'
-  // };
+  let projects = {
+    project1: 'Project 1 Index',
+    project2: 'Project 2 Search',
+    project3: 'Project 3 Threads',
+    project4: 'Project 4 Crawl',
+    project5: 'Project 5 Server'
+  };
 
-  // for (const project in projects) {
+  for (const project in projects) {
+    if (releases.hasOwnProperty(project)) {
+      summary = summary.addRaw(`## ${projects[project]}`, true);
 
-  // }
 
-  // await core.summary.addRaw(markdown.join('\n')).write();
+      summary = summary.addRaw('', true);
+    }
+  }
+
   await summary.write();
 };
