4c1392fcbadd3f630f16e57a9b674800725a1326
Sophie Engle <sjengle@cs.usfca.edu>
2023-12-10 17:36:56 -0800

    Adding table

--- .github/scripts/stats-process.js
+++ .github/scripts/stats-process.js
@@ -1,13 +1,7 @@
 module.exports = async ({github, context, core}) => {
   const releases = JSON.parse(process.env.RELEASES);
 
-  let summary = core.summary;
-  summary = summary.addRaw('# CS 272 Project Statistics', true);
-  summary = summary.addRaw('', true);
-  summary = summary.addRaw(`:octocat: Hello @${ context.actor }! You can find your project statistics below.`, true);
-  summary = summary.addRaw('', true);
-
-  let projects = {
+  const projects = {
     project1: 'Project 1 Index',
     project2: 'Project 2 Search',
     project3: 'Project 3 Threads',
@@ -15,10 +9,23 @@ module.exports = async ({github, context, core}) => {
     project5: 'Project 5 Server'
   };
 
+  const listReleases = version => `\`${version}\``;
+
+  let summary = core.summary;
+  summary = summary.addRaw('# CS 272 Project Statistics', true);
+  summary = summary.addRaw('', true);
+  summary = summary.addRaw(`:octocat: Hello @${ context.actor }! You can find your project statistics below.`, true);
+  summary = summary.addRaw('', true);
+
+
   for (const project in projects) {
     if (releases.hasOwnProperty(project)) {
-      summary = summary.addRaw(`## ${projects[project]}`, true);
+      const current = releases[project];
 
+      summary = summary.addRaw(`## ${projects[project]}`, true);
+      summary = summary.addRaw(`| **Label** | **Descriptions** |`, true);
+      summary = summary.addRaw(`|----------:|:-----------------|`, true);
+      summary = summary.addRaw(`| Project Tests: | ${current['grade-tests'].map(listReleases).join(', ')} |`, true);
 
       summary = summary.addRaw('', true);
     }
